416cc1e836f81a144e7f3580a02bf8092208413a
CatsIsMyLife-xD <andreev054@mail.ru>
2022-08-29 23:14:53 +0300

    Переделал работу генератора и обавил многопоточность

--- client.py
+++ client.py
@@ -1,5 +1,6 @@
 from opcua import Client 
 import time
+from threading import Thread
 
 url = "opc.tcp://192.168.1.23:4840" 
 
@@ -9,18 +10,25 @@ client.connect()
 
 print("Client Connected")
 
-while True:
-	
-	Water = client.get_node("ns=2;i=3")
-	Watering = Water.get_value()
-	print (Watering) 
+def main():
+    while True:
+        Water = client.get_node("ns=2;i=3")
+        Watering = Water.get_value()
+        Shutt = client.get_node("ns=2;i=4")
+        Shuttering = Shutt.get_value()
+        print (Watering, Shuttering, sep = "\t") 
+        time.sleep(1)
 
-	Shutt = client.get_node("ns=2;i=4")
-	Shuttering = Shutt.get_value()
-	print (Shuttering)
- 
-	time.sleep(1)  
- 
+def ChangeAtr():
+    txtW = input()
+    txtS = input()
+    f = open('gen.txt','w')
+    f.write(f'{txtW}\t{txtS}')
+
+th = Thread(target = main,args = ())
+th2 = Thread(target = ChangeAtr, args = ())
+th.start()
+th2.start()
 
 
 	
