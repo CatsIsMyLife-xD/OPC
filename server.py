4774d8376bd82edc7f9e76330a0f0488e9fde584
CatsIsMyLife-xD <andreev054@mail.ru>
2022-08-30 19:09:59 +0300

    Удалил все и оставил только работу с файлами

--- server.py
+++ server.py
@@ -9,20 +9,16 @@ addSpace = server.register_namespace("OPCUA Server")
 node = server.get_objects_node()
 ServerInfo = node.add_object(addSpace, "OPC Server")
 Param = node.add_object(addSpace, "Parameters")
-#Эти строки можешь вставить в код OPC
 Water = Param.add_variable(addSpace, "Watering",0)
 Shutt = Param.add_variable(addSpace, "Shutter",0)
 Water.set_writable()
 Shutt.set_writable()
-#Они спокойно интегрируются для создания одного сервера
 server.start()
 print ("{} started at {}".format(server.name, url))
 
 ChangeW, ChangeS = 0, 0
 while True:
-    os.system('python gen.py')
     file = open ('gen.txt','r')
-    #Аналогично, можно сделать один сервак OPC
     for i in range(1):
         buf = file.readline()
         a,b = buf.split('\t')
